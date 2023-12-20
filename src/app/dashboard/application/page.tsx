9bfc49ce399bdbb7888d37210ef4b8ddcfa580fd
Special <Isotoxic1@gmail.com>
2023-12-19 19:38:55 -0500

    fixed a erorr

--- src/app/dashboard/application/page.tsx
+++ src/app/dashboard/application/page.tsx
@@ -1,4 +1,3 @@
-// *TODO:
-// Show a list of people who applied to a box
-// List the jobId, application resume, appilcation answer to qusetion.
-// Allow the manager to quickly respond to the application
+export default function JobPosterEditor() {
+    return <h1>Work in progress</h1>
+}
