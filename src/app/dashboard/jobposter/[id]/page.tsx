a89f6b4a557f7068846648a11ab7a642f108dac9
Special <Isotoxic1@gmail.com>
2024-01-31 22:35:52 -0500

    Fixed issue with job posting

--- src/app/dashboard/jobposter/[id]/page.tsx
+++ src/app/dashboard/jobposter/[id]/page.tsx
@@ -4,15 +4,15 @@ export default async function JobEditor({ params }: { params: { id: string } }) 
 	const getValue = await getSingleJob(params.id)
 	return (
 		<div>
-			<div className="bg-slate-100 min-h-screen max-w-full md:w-screen">
-				<div className="min-[320px]:p-0 sm:p-3 md:p-10 md:w-2/3">
-					<h1 className="text-md md:text-lg font-semibold my-3 ">Editing: {getValue!.jobName}</h1>
-					<div className="shadow-lg overflow-y-scroll max-h-[70vh]">
+			<div className="min-h-screen max-w-full bg-slate-100 md:w-screen">
+				<div className="min-[320px]:p-0 sm:p-3 md:w-2/3 md:p-10">
+					<h1 className="text-md my-3 font-semibold md:text-lg ">Editing: {getValue!.jobName}</h1>
+					<div className="max-h-[70vh] overflow-y-scroll shadow-lg">
 						<FullJobEditorForm
 							id={getValue!.id}
 							JobName={getValue!.jobName}
-							JobDescription={getValue!.jobName}
-							JobLocation={getValue!.jobName}
+							JobDescription={getValue!.jobDescription}
+							JobLocation={getValue!.jobLocation}
 							FullTime={getValue!.fullTime}
 							RequireResume={getValue!.requireResume}
 							questions={getValue!.questions.map((question) => ({
--- src/app/dashboard/jobposter/page.tsx
+++ src/app/dashboard/jobposter/page.tsx
@@ -3,10 +3,10 @@ import FullJobEditorForm from '@/components/dashboard/poster/fullJobEditorForm'
 export default async function Application() {
 	return (
 		<>
-			<div className="bg-slate-100 min-h-screen max-w-full md:w-screen">
-				<div className="min-[320px]:p-0 sm:p-3 md:p-10 md:w-2/3">
-					<h1 className="text-md md:text-lg font-semibold my-3 ">Job Poster</h1>
-					<div className="shadow-lg overflow-y-scroll max-h-[70vh]">
+			<div className="min-h-screen max-w-full bg-slate-100 md:w-screen">
+				<div className="min-[320px]:p-0 sm:p-3 md:w-2/3 md:p-10">
+					<h1 className="text-md my-3 font-semibold md:text-lg ">Job Poster</h1>
+					<div className="max-h-[70vh] overflow-y-scroll shadow-lg">
 						<FullJobEditorForm
 							JobName={''}
 							JobDescription={''}
