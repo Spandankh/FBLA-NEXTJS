5023fd1d270dadc78089f1c148590fd43cb932f1
Special <Isotoxic1@gmail.com>
2024-01-31 21:06:04 -0500

    Fixed fav icon & navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -23,7 +23,9 @@ const NavBar = () => {
 
 	return (
 		<nav className="border-white-1 font-equinox-sans left-0 top-0 z-30 mx-auto flex h-auto   w-full items-center justify-between bg-[#171717] text-[#f2f2f2]">
-			<h1 className="w-full px-4 text-3xl font-bold">WARIO</h1>
+			<Link href="/" className="w-full px-4 text-3xl font-bold">
+				WARIO
+			</Link>
 			<ul className="mr-auto hidden uppercase md:flex">
 				<Link className="p-4" href="/">
 					Home
@@ -72,31 +74,6 @@ const NavBar = () => {
 					</SheetHeader>
 				</SheetContent>
 			</Sheet>
-
-			{/* <div
-                className={
-                    !nav
-                        ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500'
-                        : 'fixed left-[-100%] top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500'
-                }
-            >
-                <h1 className="w-full text-3xl font-bold m-4 ">WARIO</h1>
-                <ul className="pt-24 uppercase">
-                    <li className="p-4 border-b border-gray-600">
-                        <Link href="/home">HOME</Link>
-                    </li>
-                    <li className="p-4 border-b border-gray-600 ">
-                        <Link href="/about"> About Us</Link>
-                    </li>
-                    <li className="p-4 border-b border-gray-600">
-                        <Link href="/career">CAREERS</Link>
-                    </li>
-                    <li className="p-4 border-b border-gray-600">
-                        <Link href="/dashboard">dashboard</Link>
-                    </li>
-                    <li className="p-4 border-b border-gray-600"> Settings</li>
-                </ul>
-            </div> */}
 		</nav>
 	)
 }
